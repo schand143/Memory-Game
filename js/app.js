06974a88f3b82745a9a8fc2373edc09324fe2b73
schand143 <shaikhchand724@gmail.com>
2019-12-24 20:32:00 +0530

    Half of code complited

--- js/app.js
+++ js/app.js
@@ -1,7 +1,144 @@
 /*
  * Create a list that holds all of your cards
  */
+const icons = [
+  "fa fa-diamond",
+  "fa fa-diamond",
+  "fa fa-paper-plane-o",
+  "fa fa-paper-plane-o",
+  "fa fa-anchor",
+  "fa fa-anchor",
+  "fa fa-bolt",
+  "fa fa-bolt",
+  "fa fa-cube",
+  "fa fa-cube",
+  "fa fa-leaf",
+  "fa fa-leaf",
+  "fa fa-bicycle",
+  "fa fa-bicycle",
+  "fa fa-bomb",
+  "fa fa-bomb"
+];
 
+const cardContainer = document.querySelector(".deck");
+
+let openedCard = [];
+let matchedCard = [];
+
+/*
+ * Initialize the game
+ */
+
+function init() {
+  // Create the cards
+  for (let i = 0; i < icons.length; i++) {
+    const card = document.createElement("li");
+    card.classList.add("card");
+    //   card.innerHTML = "<i class='" + icons[i] + "'</i>";  old syntax
+    card.innerHTML = `<i class="${icons[i]}"></i>`;
+    cardContainer.appendChild(card);
+
+    // Add click  event to each card
+    click(card);
+  }
+}
+
+/*
+ * Click event listener
+ */
+
+function click(card) {
+  // Card Click Event
+  card.addEventListener("click", function() {
+    const currentCard = this;
+    const previousCard = openedCard[0];
+    // console.log(card.innerHTML);
+
+    //we have an existing OPENED cards
+
+    if (openedCard.length === 1) {
+      card.classList.add("open", "show", "disable");
+      openedCard.push(this);
+
+      // we should compare our 2 opened cards!
+      compare(currentCard, previousCard);
+    } else {
+      //we don't have an existing OPENED cards
+      currentCard.classList.add("open", "show", "disable");
+      openedCard.push(this);
+    }
+  });
+}
+
+/*
+ * Compare the 2 cards!
+ */
+
+function compare(currentCard, previousCard) {
+  if (currentCard.innerHTML === previousCard.innerHTML) {
+    // Matched
+    currentCard.classList.add("match");
+    previousCard.classList.add("match");
+
+    matchedCard.push(currentCard, previousCard);
+    //Reset Card
+    openedCard = [];
+    // console.log("Matched!");
+
+    // Check if game is over !
+    isOver();
+  } else {
+    // wait for 500ms then, do this!
+    setTimeout(function() {
+      // Doesn't Match
+      currentCard.classList.remove("open", "show", "disable");
+      previousCard.classList.remove("show", "show", "disable");
+      // Reset Card
+      openedCard = [];
+      // console.log("Doesn't Matched!");
+    }, 500);
+  }
+}
+
+/*
+ * Check if the game is over!
+ */
+
+function isOver() {
+  if (matchedCard.length === icons.length) {
+    alert("Congratulation !!! Game is Over");
+  }
+}
+
+/*
+ * Add moves!
+ */
+
+let moves = 0;
+
+function addMoves() {
+  moves++;
+}
+
+/*
+ * Restart the button!
+ */
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function() {
+  // Delete all cards !
+
+  cardContainer.innerHTML = "";
+  //Call `init` to create new cards !
+
+  init();
+
+  // Reset any RELATED variable!
+  matchedCard = [];
+});
+
+/*-------- Start the game first time !!!!!--------------*/
+init();
 
 /*
  * Display the cards on the page
@@ -12,19 +149,20 @@
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
